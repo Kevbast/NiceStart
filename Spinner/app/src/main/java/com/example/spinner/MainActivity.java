09d7999ffa0a0902161d1d221ac350fce66428a3
Kevin <Kevbast936@gmail.com>
2023-11-02 12:52:37 +0100

    Buttons and themes

--- Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,12 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         private Spinner dias;
         private TextView mensa;
+        private CheckBox Basket;
+        private CheckBox Futb;
+
+        //Creamos variable radioboton
+        private RadioButton cris,messi;
+        private RadioGroup grupo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mensa=findViewById(R.id.Mensaje);
         dias=findViewById(R.id.dias_semana);
+        Basket=findViewById(R.id.basket);
+        Futb=findViewById(R.id.fut);
+        //Declaramos radio----------------------------
+        cris=findViewById(R.id.Cris);
+        messi=findViewById(R.id.Messi);
+        grupo=findViewById(R.id.radioG);
         //Creamos un array de los datos que queremosmque muestre el spinner
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(
                 this,R.array.diasemana2, android.R.layout.simple_spinner_item );
@@ -40,6 +57,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+           //PARA VER QUE HAS SELECCIONADO LA CHECKBOX
+        Basket.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    mensa.setText("Has seleccionado: "+ Basket.getText());
+                }else
+                    mensa.setText(" ");
+            }
+        });
+        Futb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    mensa.setText("Has seleccionado: "+ Futb.getText());
+                }else
+                    mensa.setText(" ");
+            }
+        });
+
+        grupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.Cris){
+                    mensa.setText("Tu jugador favorito es: "+ cris.getText());
+                }else if(checkedId==R.id.Messi)
+                    mensa.setText("Tu jugador favorito es: "+ messi.getText());
+            }
+        });
     }
 
 }
