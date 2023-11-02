9f0b36a4d6db6b32bde00d131ddb498f037789f9
Kevin <Kevbast936@gmail.com>
2023-11-02 13:57:41 +0100

    Added activity_register

--- STOP/app/src/main/java/com/example/nicestart/Register.java
+++ STOP/app/src/main/java/com/example/nicestart/Register.java
@@ -1,16 +1,14 @@
 package com.example.nicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    Button registro;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        registro=findViewById(R.id.signup);
-
+        setContentView(R.layout.activity_register);
     }
 }
