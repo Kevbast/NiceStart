693c1e7dbbb364d65e21e0813230c94f702a7df9
Kevin <Kevbast936@gmail.com>
2023-11-26 19:44:38 +0100

    Perdir_cita modificado

--- Pedir_cita/app/src/main/java/com/example/pedir_cita/MainActivity.java
+++ Pedir_cita/app/src/main/java/com/example/pedir_cita/MainActivity.java
@@ -16,6 +16,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
     private CalendarView calendar;
     private TextView date_view;
+    int dayOfWeek,Date;
     private Button enviar;
     private EditText dni;
     private EditText hora;
@@ -29,27 +30,31 @@ public class MainActivity extends AppCompatActivity {
         enviar= findViewById(R.id.enviar);
         dni= findViewById(R.id.dniIntro);
         hora= findViewById(R.id.hora);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, dayOfMonth);
 
+                dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+                String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
+
+            }
+        });
          enviar.setOnClickListener(new View.OnClickListener() {
+             String palabra =hora.getText().toString();
+             int hora1=Integer.parseInt(palabra);
              @Override
              public void onClick(View v) {
-                 calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-                     @Override
-                     public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-                         Calendar calendar = Calendar.getInstance();
-                         calendar.set(year, month, dayOfMonth);
+                 if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+                     errorfecha();
+                 } else if(dni.getText().length()!=9){
 
-                         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-
-                         if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
-                             errorfecha();
-                         } else {
-                             String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
-                             date_view.setText(Date);
-                         }
+                     errorDNI();
+                 }else if(!(hora1 >= 9 && hora1 <= 14)){
+                    errorHora();
                      }
-                 });
-             }
+                 }
          });
     }
 
@@ -61,6 +66,34 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"La hora indicada está mal",Toast.LENGTH_SHORT).show();
     }
     protected void errorDNI(){
+        /*public class ValidadorDNI {
+            private static final String LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+            public static boolean validar(String dni) {
+                // Comprueba si el DNI tiene 9 caracteres
+                if (dni.length() != 9) {
+                    return false;
+                }
+
+                // Extrae los números y la letra del DNI
+                String numeros = dni.substring(0, 8);
+                char letra = Character.toUpperCase(dni.charAt(8));
+
+                // Comprueba si la parte numérica son solo dígitos
+                try {
+                    int numeroDNI = Integer.parseInt(numeros);
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+
+                // Calcula la letra del DNI
+                int indice = Integer.parseInt(numeros) % 23;
+                char letraCalculada = LETRAS_DNI.charAt(indice);
+
+                // Comprueba si la letra calculada coincide con la letra del DNI
+                return letra == letraCalculada;
+            }
+        }*/
         Toast.makeText(this,"El DNI está mal",Toast.LENGTH_SHORT).show();
     }
     protected void mensaEnviado(){
